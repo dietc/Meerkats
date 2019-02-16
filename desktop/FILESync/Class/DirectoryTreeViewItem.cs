fb77fea908f1a571bcfbc5993e4edac789b57f33
Yenan Huang <yenan.huang@kcl.ac.uk>
2019-02-16 15:33:38 +0000

    delete some useless using

--- desktop/FILESync/Class/DirectoryTreeViewItem.cs
+++ desktop/FILESync/Class/DirectoryTreeViewItem.cs
@@ -1,9 +1,6 @@
 ﻿using System;
 using System.IO;
 using System.Windows;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Controls;
 using System.Windows.Media.Imaging;
 
 namespace FILESync.Class
--- desktop/FILESync/Class/JsonHeaderLogic.cs
+++ desktop/FILESync/Class/JsonHeaderLogic.cs
@@ -1,9 +1,7 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Newtonsoft.Json.Linq;
 
 
 namespace FILESync.Class
