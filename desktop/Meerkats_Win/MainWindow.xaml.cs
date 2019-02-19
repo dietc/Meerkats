c0890c1a3ed0eabb428e474c06227eae17c7c5f7
921 Huang <yenan.huang@kcl.ac.uk>
2019-02-19 12:56:53 +0000

    version 1.10

---
+++ desktop/Meerkats_Win/MainWindow.xaml.cs
@@ -0,0 +1,136 @@
+﻿using Meerkats_Win.Class;
+using System;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Meerkats_Win
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public ObservableCollection<FileInfo_cs> List = new ObservableCollection<FileInfo_cs>();
+        public MainWindow()
+        {
+            InitializeComponent();
+            init_status();
+        }
+
+        public void init_status()
+        {
+
+            file_tree.Items.Clear();
+            file_tree.ItemsSource = List;
+
+            //// Json Filedata
+            //string File_json = "{\"1\":\"2\",\"3\":{\"4\":\"5\",\"6\":\"7\"}}";
+
+            //var js_obj = JObject.Parse(File_json);
+            ////创建TreeView的数据源
+            //file_tree.ItemsSource = js_obj.Children().Select(c => JsonHeaderLogic.FromJToken(c));
+
+            DirectoryTreeView mainTree = new DirectoryTreeView();
+            mainTree.SelectedItemChanged += MainTree_SelectedItemChanged;
+            file_tree_grid.Children.Add(mainTree);
+
+            //右键菜单
+            ContextMenu myContext = new ContextMenu();
+
+            MenuItem myMUItem = new MenuItem();
+            myMUItem.Header = "Open";
+            myMUItem.Name = "Menu01";
+            myContext.Items.Add(myMUItem);
+
+            myMUItem = new MenuItem();
+            myMUItem.Header = "View";
+            myMUItem.Name = "Menu02";
+            //myMUItem.Click += FileLook_Click;
+            myContext.Items.Add(myMUItem);
+
+            myMUItem = new MenuItem();
+            myMUItem.Header = "Refresh";
+            myMUItem.Name = "Menu03";
+            myContext.Items.Add(myMUItem);
+
+            myMUItem = new MenuItem();
+            myMUItem.Header = "Rename";
+            myMUItem.Name = "Menu04";
+            myContext.Items.Add(myMUItem);
+
+            myMUItem = new MenuItem();
+            myMUItem.Header = "Delete";
+            myMUItem.Name = "Menu05";
+            myContext.Items.Add(myMUItem);
+
+
+            myMUItem = new MenuItem();
+            myMUItem.Header = "New directory";
+            myMUItem.Name = "Menu06";
+            myContext.Items.Add(myMUItem);
+
+            //myMUItem = new MenuItem();
+            //myMUItem.Header = "Upload file";
+            //myMUItem.Click += upload_file;
+            //myMUItem.Name = "Menu07";
+            //myContext.Items.Add(myMUItem);
+
+            file_info.ContextMenu = myContext;
+
+        }
+
+        private void upload_file(object sender, RoutedEventArgs e)
+        {
+            //
+        }
+
+        /// <summary>
+        /// 文件夹树改变时，查找文件夹下是否存在文件，如果存在，则显示
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void MainTree_SelectedItemChanged(object sender, RoutedPropertyChangedEventArgs<object> e)
+        {
+            DirectoryTreeViewItem item = e.NewValue as DirectoryTreeViewItem;
+
+            //stack.Children.Clear();
+            file_info.Items.Clear();
+
+            FileInfo[] fileInfos;
+
+            try
+            {
+                fileInfos = item.DirInfo.GetFiles();
+            }
+            catch
+            {
+                return;
+            }
+
+            foreach (FileInfo info in fileInfos)
+            {
+                FileInfo_cs myFile = new FileInfo_cs();
+                myFile.strFileName = info.Name;
+                myFile.strFileType = info.Extension;
+                myFile.strFileSize = info.Length.ToString();
+                myFile.strlastModifyTime = info.LastAccessTime.ToString();
+                file_info.Items.Add(myFile);
+            }
+        }
+
+
+        // connect to the server or refresh
+        private void Conect_btn_Click(object sender, RoutedEventArgs e)
+        {
+            Tcp_connect t1 = new Tcp_connect();
+            string status = t1.tcp_send("178.128.45.7", 4356, "hello");
+            fortest.Text = status;
+        }
+    }
+}
