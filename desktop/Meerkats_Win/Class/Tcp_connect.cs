5cc264ac941d42a1150e2a79c66e27561d04d2bd
921 Huang <yenan.huang@kcl.ac.uk>
2019-02-21 17:16:16 +0000

    Signed-off-by: 921 Huang <yenan.huang@kcl.ac.uk>

--- desktop/Meerkats_Win/Class/Tcp_connect.cs
+++ desktop/Meerkats_Win/Class/Tcp_connect.cs
@@ -6,6 +6,7 @@ using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 using System.Security.Cryptography;
+using System.IO;
 
 namespace Meerkats_Win.Class
 {
@@ -22,27 +23,28 @@ namespace Meerkats_Win.Class
                 {
                     uint content_length = (uint)(data.Length + 2);
                     uint total_length = (uint)(content_length + 28);
+
                     string testdata = null;
-                    //initiator  -- 8 
-                    byte[] initiator = { 0x11, 0xff, 0x6c, 0x6f, 0x6e, 0x64, 0x6f, 0x6e };
-                    testdata += System.Text.Encoding.Default.GetString(initiator);
-                    //length -- 2 
-                    byte[] length = { (byte)(uint)(content_length >> 8), (byte)(uint)(content_length & 0xff) };
-                    testdata += System.Text.Encoding.Default.GetString(length);
-                    // type  -- 1
-                    byte[] type = {0x1};
-                    testdata += System.Text.Encoding.Default.GetString(type);
-                    //device id -- 1
-                    byte[] device_id = { 0x1 };
-                    testdata += System.Text.Encoding.Default.GetString(device_id);
-                    //context
-                    testdata += data;
-                    //checksum
-                    string checksum = data + "aaaaa";
-                    testdata += GetMd5Str(checksum);
-                    //end
-                    byte[] end = { 0xff, 0xee };
-                    testdata += System.Text.Encoding.Default.GetString(end);
+                    ////initiator  -- 8 
+                    //byte[] initiator = { 0x11, 0xff, 0x6c, 0x6f, 0x6e, 0x64, 0x6f, 0x6e };
+                    //testdata += System.Text.Encoding.Default.GetString(initiator);
+                    ////length -- 2 
+                    //byte[] length = { (byte)(uint)(content_length >> 8), (byte)(uint)(content_length & 0xff) };
+                    //testdata += System.Text.Encoding.Default.GetString(length);
+                    //// type  -- 1
+                    //byte[] type = { 0x1 };
+                    //testdata += System.Text.Encoding.Default.GetString(type);
+                    ////device id -- 1
+                    //byte[] device_id = { 0x1 };
+                    //testdata += System.Text.Encoding.Default.GetString(device_id);
+                    ////context
+                    //testdata += data;
+                    ////checksum
+                    //string checksum = data + "aaaaa";
+                    //testdata += GetMd5Str(checksum);
+                    ////end
+                    //byte[] end = { 0xff, 0xee };
+                    //testdata += System.Text.Encoding.Default.GetString(end);
 
                     Byte[] bytSend = Encoding.UTF8.GetBytes(testdata);
                     ntwStream.Write(bytSend, 0, bytSend.Length);
@@ -60,6 +62,62 @@ namespace Meerkats_Win.Class
             {
                 return ("Error:" + ex.Message);
             }
+        }
+
+        public byte[] BuildDataPackage(byte[] MessageBody)
+        {
+            int MessageBodyLength = 0;
+            MessageBodyLength = MessageBody.Length;
+
+            byte[] MessageBodyByte = new byte[MessageBodyLength + 30];
+
+            // Packet Initiator[ 8 bytes] 0x11 0xff 0x6c 0x6f 0x6e 0x64 0x6f 0x6e
+            byte[] Initiator = { 0x11, 0xff, 0x6c, 0x6f, 0x6e, 0x64, 0x6f, 0x6e };
+            Buffer.BlockCopy(Initiator, 0, MessageBodyByte, 0, Initiator.Length);
+
+            // Packet Content Length [ 2 bytes ]
+            byte[] length = { (byte)(uint)((MessageBodyLength + 2) >> 8), (byte)((MessageBodyLength + 2) & 0xff) };
+            Buffer.BlockCopy(length, 0, MessageBodyByte, 8, length.Length);
+            // Packet Type [ 1 byte ]
+            MessageBodyByte[10] = 0x1;
+            // Packet ID [ 1 byte ]
+            MessageBodyByte[11] = 0x1;
+            // Packet Content [ len(data) bytes ]
+            Buffer.BlockCopy(MessageBody, 0, MessageBodyByte, 12, MessageBody.Length);
+            // Md5 Checksum [16 bytes]
+            byte[] Check_sum = new byte[MessageBodyLength + 5];
+            Buffer.BlockCopy(MessageBody, 0, Check_sum, 0, MessageBody.Length);
+            
+            // private_key = 'aaaaa'
+            byte[] private_key = {0x61, 0x61, 0x61, 0x61, 0x61 };
+            Buffer.BlockCopy(private_key, 0, Check_sum, MessageBody.Length, private_key.Length);
+
+            string str = System.Text.Encoding.Default.GetString(Check_sum);
+            byte[] md5 = System.Text.Encoding.Default.GetBytes(GetMd5Str(str)); 
+            Buffer.BlockCopy(md5, 0, MessageBodyByte, 12 + MessageBody.Length, md5.Length);
+
+            MessageBodyByte[MessageBodyLength + 28] = 0xff;
+            MessageBodyByte[MessageBodyLength + 29] = 0xee;
+            return (MessageBodyByte);
+
+        }
+
+        public string tcp_listen(string ip,int port)
+        {
+            Socket listener = new Socket(AddressFamily.InterNetwork,
+                SocketType.Stream, ProtocolType.Tcp);
+            listener.Bind(new IPEndPoint(IPAddress.Any, port));
+
+            //listen
+            string data;
+            
+            listener.Listen(0);
+            Socket socket = listener.Accept();
+            NetworkStream ntwStream = new NetworkStream(socket);
+            StreamReader strmReader = new StreamReader(ntwStream);
+            data = strmReader.ReadToEnd();
+            socket.Close();           
+            return (data);
         }
 
         /// <summary>
