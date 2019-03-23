9f488f71ad9cb5ed56ae0ad9aa71ace8c6e6321c
k1800566 <xi.he@kcl.ac.uk>
2019-03-23 19:55:28 +0000

    DOWNLOAD FEATURE ADDED

---
+++ android/meerkats/app/src/main/java/com/example/meerkats/FileIndexJson.java
@@ -0,0 +1,35 @@
+package com.example.meerkats;
+
+
+    public class FileIndexJson {
+
+         String Name;
+         Byte Num;
+         Byte Index;
+         String getName() {
+            return Name;
+        }
+
+        public void setName(String name) {
+            Name = name;
+        }
+
+        public Byte getNum() {
+            return Num;
+        }
+
+        public void setNum(Byte num) {
+            Num = num;
+        }
+
+        public Byte getIndex() {
+            return Index;
+        }
+
+        public void setIndex(Byte index) {
+            Index = index;
+        }
+    }
+
+
+
